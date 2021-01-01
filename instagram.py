5faf7b8951f93b796c6ab12f4ad6beb255e63d8b
BoxDelleIdee <76827567+BoxDelleIdee@users.noreply.github.com>
2021-01-01 22:39:49 +0100

    Messo path cablato

--- instagram.py
+++ instagram.py
@@ -8,7 +8,7 @@ L.login(user, password)
 # Richiedo le informazioni
 profile = instaloader.Profile.from_username(L.context, user)
 # Apro il file dove scrivere i dati
-f= open("instagram.txt","w+")
+f= open("/home/pi/Cornice/instagram.txt","w+")
 # Scrivo solo l'informazione dei followers
 f.write(str(profile.followers))
 f.close()
